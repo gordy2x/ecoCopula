95175bcbee5954c72e6b4d1a2267927fd00c1095
Gordana Popovic <g.popovic@unsw.edu.au>
2019-03-01 14:42:50 +1100

    fixed scaling etc on ordiplot

--- R/plot_cord.R
+++ R/plot_cord.R
@@ -1,5 +1,3 @@
-
-
 #' Plots an ordination plot of latent variables and their corresponding coefficients (biplot).
 #'
 #' @param obj is a cord object, e.g. from output of \code{cord}
@@ -19,7 +17,7 @@
 #' @export ordiplot
 ordiplot<- ordiplot
 #' @export ordiplot.cord
-ordiplot.cord = function(obj, biplot = FALSE, ...) {
+ordiplot.cord = function(obj, biplot = FALSE,site.col="black",sp.col="black",alpha=0.9,arrow=TRUE, ...) {
     
     #species labels from the original data
     labs = colnames(obj$obj$data$abund)
@@ -29,18 +27,20 @@ ordiplot.cord = function(obj, biplot = FALSE, ...) {
     scores=t(obj$scores[[1]][1:2,])
   
     #calculate scaling factor
-    alpha=sqrt(max(apply(scores^2,1,sum)))/sqrt(max(apply(loadings^2,1,sum)))*0.9
+    alpha_plot=sqrt(max(apply(scores^2,1,max)))/sqrt(max(apply(loadings^2,1,max)))*alpha
     
     #plot graph
     plot(scores,pch=16,ylab="Latent variable 2",
-         xlab="Latent variable 1", type='n')
+         xlab="Latent variable 1", type='n',...)
     abline(h=0,col="gray")
     abline(v=0,col="gray")
-    text(scores,label=1:nrow(scores),...)
+    text(scores,label=1:nrow(scores),col=site.col)
     if(biplot){
-      loadings=loadings*alpha
-      # arrows(0,0,loadings[,1],loadings[,2],length=0,col="dark gray")
-      text(loadings[,1],loadings[,2],labels = labs,col="blue",cex = 0.7)
+      loadings=loadings*alpha_plot
+      if(arrow){
+        arrows(0,0,loadings[,1],loadings[,2],length=0,col="dark gray")
+      }
+      text(loadings[,1],loadings[,2],labels = labs,col="blue",cex = 0.8)
     }
     
 }
