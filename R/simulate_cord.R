84fc97a7f839d4796c70f6a6f576033702320f0c
lsjmichelle <54560395+lsjmichelle@users.noreply.github.com>
2020-09-30 14:55:29 +1000

    add newdata, reshape args (#4)
    * add newdata, reshape args
    
    * update docstring and examples
    
    * update stop message
    
    * update predict.manylm, examples
    
    * use predict.manyglm to avoid error if nlevels(newdata) differ
    
    * fix simulate_newY
    
    * add coeffs arg, use predict
    
    * output matrix; rm reshape2matrix & coeffs args
    
    * fix bug in single column df with '.' in colname

--- R/simulate_cord.R
+++ R/simulate_cord.R
@@ -1,19 +1,29 @@
 #'Simulates new data from a given cord object
 #'
-#' @param obj is a cord object, e.g. from output of \code{cord}
-#' @param nsim number of simulations, defaults to 1
-#' @param seed random number seed
+#' @param object is a cord object, e.g. from output of \code{cord}
+#' @param nsim Number of simulations, defaults to 1. If nsim > 1, the simulated data will be
+#' appended.
+#' @param seed Random number seed, defaults to a random seed number.
+#' @param newdata A data frame in which to look for X covariates with which to simulate.
+#' Defaults to the X covariates in the fitted model.
 #' @export
 #' @examples
 #' data(spider)
-#' abund <- mvabund(spider$abund)
-#' X <- spider$x
-#' spider_mod=manyglm(abund~1)
-#' spid_lv=cord(spider_mod)
+#' abund = mvabund(spider$abund)
+#' X = data.frame(spider$x)
+#'
+#' spider_mod = manyglm(abund~1)
+#' spid_lv = cord(spider_mod)
 #' simulate(spid_lv)
+#'
+#' spider_mod_X = manyglm(abund ~ soil.dry + bare.sand, data=X)
+#' spid_lv_X = cord(spider_mod_X)
+#' Xnew = X[1:10,]
+#' simulate(spid_lv_X, newdata = Xnew)
+#' simulate(spid_lv_X, nsim=2, newdata = Xnew)
 
 
-simulate.cord = function(fit, nsim = 1, seed = NULL) {
+simulate.cord = function(object, nsim=1, seed=NULL, newdata=object$obj$data) {
   
   # code chunk from simulate.lm to select seed
   if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) 
@@ -26,49 +36,74 @@ simulate.cord = function(fit, nsim = 1, seed = NULL) {
     RNGstate <- structure(seed, kind = as.list(RNGkind()))
     on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
   }
-  
-  # generate new data
-  ys <- array(NaN, c(nrow(fit$obj$y), ncol(fit$obj$y), nsim))
-  if (fit$obj$call[[1]] == "manyglm") {
-    prs <- suppressWarnings(predict.manyglm(fit$obj, type = "response")) # warning for family=poisson suppressed
-  }
-  if (fit$obj$call[[1]] == "manylm") {
-    prs <- predict.manylm(fit$obj, type = "response")
-  }
-  
-  for (k in c(1:nsim)) {
-    # simulate multivariate normal random variables
-    sim <- MASS::mvrnorm(nrow(fit$obj$y), mu = rep(0, times = ncol(fit$obj$y)), fit$sigma[[1]])
-    # turn simulated variables into abundances
-    if (fit$obj$call[[1]] == "manyglm") {
-      if (fit$obj$family == "negative.binomial") {
-        size <- fit$obj$theta
-        for (j in c(1:ncol(fit$obj$y))) {
-          ys[,j,k] <- qnbinom(pnorm(sim[,j]), size = size[j], mu = prs[,j])
-        }
-      } else if (fit$obj$family == "poisson") {
-        for (j in c(1:ncol(fit$obj$y))) {
-          ys[,j,k] <- qpois(pnorm(sim[,j]), lambda = prs[,j])
-        }
-      } else if (fit$obj$call$family == "binomial") {
-        for (j in c(1:ncol(fit$obj$y))) {
-          ys[,j,k] <- qbinom(pnorm(sim[,j]), size = 1, prob = prs[,j])
-        }
-      } else {
-        stop (paste("'family'", fit$obj$family, "is not supported currently."))
-      }
-    } else if (fit$obj$call[[1]] == "manylm") {
-      df.residual <- fit$obj$df.residual
-      sigma2 <- apply((fit$obj$y - fit$obj$fitted)^2, 2, sum)/df.residual
-      for (j in c(1:ncol(fit$obj$y))) {
-        ys[,j,k] <- qnorm(pnorm(sim[,j]), mean = prs[,j], sd = sqrt(sigma2[j]))
-        # ys[,j,k] <- rnorm(ys[,j,k], mean = prs[,j], sd = sqrt(sigma2[j]))
-      }
+
+  newdata = reshape_newdata(object, nsim, newdata)
+  prs = suppressWarnings(
+      predict(object$obj, type = "response", newdata = newdata)
+  ) # warning for family=poisson suppressed
+  newY = simulate_newY(object, prs)
+
+  return (newY)
+}
+
+reshape_newdata = function(object, nsim, newdata) {
+  if (formula(object$obj)[-2] == ~1) {
+    newdata = data.frame(array(1, nrow(newdata)*nsim))
+  } else {
+    if (nsim == 1) {
+      newdata = newdata
     } else {
-      stop (paste("'function'", fit$obj$call[[1]], "is not supported currently."))
+      if (ncol(newdata) > 1) {
+        newdata = do.call("rbind", replicate(nsim, newdata, simplify = FALSE))
+      } else {
+        name = colnames(newdata)
+        colnames(newdata) = "V1"
+        newdata = data.frame(rep(newdata, nsim))
+
+        if (ncol(newdata) > 1) {
+          newdata = reshape(newdata, direction = "long", varying=1:ncol(newdata))["V1"]
+        }
+        colnames(newdata) = name
+      }
     }
   }
-  
-  colnames(ys) <- colnames(fit$obj$y)
-  return (ys)
+
+ return (newdata)
+}
+
+simulate_newY = function(object, prs) {
+  nRow = nrow(prs)
+  nVar = ncol(prs)
+  newY = matrix(NA, nRow, nVar)
+
+  # simulate multivariate normal random variables
+  sim = MASS::mvrnorm(nRow, mu = rep(0, times = nVar), object$sigma)
+
+  # turn simulated variables into abundances
+  if (object$obj$call[[1]] == "manyglm") {
+    for (iVar in 1:nVar) {
+      if (object$obj$family == "negative.binomial") {
+        size = object$obj$theta
+        newY[,iVar] = qnbinom(pnorm(sim[,iVar]), size = size[iVar], mu = prs[,iVar])
+      } else if (object$obj$family == "poisson") {
+        newY[,iVar] = qpois(pnorm(sim[,iVar]), lambda = prs[,iVar])
+      } else if (object$obj$call$family == "binomial") {
+        newY[,iVar] = qbinom(pnorm(sim[,iVar]), size = 1, prob = prs[,iVar])
+      } else {
+        stop("'family'", object$obj$family, "not recognized")
+      }
+    }
+  } else if (object$obj$call[[1]] == "manylm") {
+    df.residual = object$obj$df.residual
+    sigma2 = apply((object$obj$y - object$obj$fitted)^2, 2, sum)/df.residual
+    for (iVar in 1:nVar) {
+      newY[,iVar] = sim[,iVar] * sqrt(sigma2[iVar])
+    }
+    newY = newY + prs
+  } else {
+    stop("'class'", class(object$obj), "not supported")
+  }
+
+  colnames(newY) = colnames(object$obj$y)
+  return (newY)
 }
